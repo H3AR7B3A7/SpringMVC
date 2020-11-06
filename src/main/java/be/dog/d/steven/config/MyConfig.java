215727696b064b2de58ca409a4bd3699b079573f
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-11-06 12:46:53 +0100

    :alembic: web.xml -> Annotation

---
+++ src/main/java/be/dog/d/steven/config/MyConfig.java
@@ -0,0 +1,10 @@
+package be.dog.d.steven.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("be.dog.d.steven")
+public class MyConfig {
+
+}
